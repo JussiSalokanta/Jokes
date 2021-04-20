5be0b00eb3b708fee7b387dec7062c7c1f784ec2
Jussi Salokanta <jussi.salokanta@tuni.fi>
2021-04-20 09:53:08 +0300

    initial push

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Well hello there</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3295A0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
