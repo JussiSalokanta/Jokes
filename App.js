abad224ca6b094700b43e1300898442cd1b6de88
Jussi Salokanta <jussi.salokanta@tuni.fi>
2021-04-25 11:57:23 +0300

    First working version

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, ActivityIndicator, FlatList, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Well hello there</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  console.log(data)
+  
+
+  useEffect(() => {
+    fetch('https://v2.jokeapi.dev/joke/Any?format=json')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          {isLoading ? <ActivityIndicator /> : (
+            <FlatList
+              data={[data]}
+              keyExtractor={({ id }, index) => id.toString()}
+              renderItem={({ item }) => (
+                <Text>
+                  {item.category},
+                  {item.type},
+                  {item.setup}
+                  {item.delivery}
+                  {item.joke}
+                </Text>
+
+              )}
+            />
+          )}
+        </View>
+      </SafeAreaView>
+    );
+  };
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#3295A0',
-    alignItems: 'center',
     justifyContent: 'center',
-  }
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+    marginTop: 30,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    marginHorizontal: 10,
+    marginTop: 20,
+  },
+  category: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    marginHorizontal: 10,
+    marginTop: 20,
+  },
 });
