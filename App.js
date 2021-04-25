61496642ab4034e6ba8822f2793ca91085613b52
Jussi Salokanta <jussi.salokanta@tuni.fi>
2021-04-25 13:40:33 +0300

    style upgrades

--- App.js
+++ App.js
@@ -1,44 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, ActivityIndicator, FlatList, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, ActivityIndicator, FlatList, Text, View, StatusBar } from 'react-native';
 
 export default App = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   console.log(data)
-  
+
 
   useEffect(() => {
-    fetch('https://v2.jokeapi.dev/joke/Any?format=json')
+    fetch('https://v2.jokeapi.dev/joke/Any')
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   }, []);
 
-  
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={styles.container}>
-          {isLoading ? <ActivityIndicator /> : (
-            <FlatList
-              data={[data]}
-              keyExtractor={({ id }, index) => id.toString()}
-              renderItem={({ item }) => (
-                <Text>
-                  {item.category},
-                  {item.type},
-                  {item.setup}
-                  {item.delivery}
-                  {item.joke}
-                </Text>
 
-              )}
-            />
+  return (
+     <SafeAreaView style={{ flex: 1 }}>
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text style={{ width: '100%', paddingTop: 10, paddingBottom: 10, textAlign: 'center', fontSize: 15 }}>Joke:</Text>
+      {isLoading ? <ActivityIndicator /> : (
+        <FlatList
+          data={[data]}
+          keyExtractor={({ id }, index) => id.toString()}
+          renderItem={({ item }) => (
+            <View>
+              
+              <Text style={{marginLeft:5}}>Category: {item.category} </Text>
+              <Text style={{marginLeft:5}}>{item.joke}</Text>
+              <Text style={{marginLeft:5}}>{item.setup}</Text>
+              <Text style={{marginLeft:5}}>{item.delivery}</Text>
+            </View>
           )}
-        </View>
+        />
+      )}
+    </View>
       </SafeAreaView>
-    );
-  };
+  );
+};
 
 
 
@@ -46,6 +47,7 @@ export default App = () => {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#77868d",
     justifyContent: 'center',
     flex: 1,
     marginLeft: 10,
@@ -55,7 +57,7 @@ const styles = StyleSheet.create({
   },
   item: {
     padding: 10,
-    fontSize: 18,
+    fontSize: 10,
     height: 44,
     marginHorizontal: 10,
     marginTop: 20,
@@ -67,4 +69,9 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginTop: 20,
   },
+  row: {
+    flex: 1,
+    padding: 4,
+
+  }
 });
