98736b86eab5eaffe6fe57cd80c68d2beac25239
Jussi Salokanta <jussi.salokanta@tuni.fi>
2021-05-05 15:09:29 +0300

    Major UI improvments and other functionalities

--- App.js
+++ App.js
@@ -1,51 +1,89 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, ActivityIndicator, FlatList, Text, View, StatusBar, Button } from 'react-native';
+import { SafeAreaView, StyleSheet, ActivityIndicator, FlatList, Text, View, StatusBar, Button, Alert } from 'react-native';
 
 export default App = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  const [fetcher, setFetcher] = useState("");
+  const [jokeType, setJokeType] = useState("");
 
-
-
-
-
+ 
   console.log(data)
 
-
+  
   useEffect(() => {
-    fetch('https://v2.jokeapi.dev/joke/' + fetcher)
+    fetch('https://v2.jokeapi.dev/joke/' + jokeType)
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
-  }, [fetcher]);
+  }, [jokeType]);
 
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      
       <View style={styles.container}>
+      
         <StatusBar barStyle="dark-content" />
-        <Text style={{ width: '100%', paddingTop: 10, paddingBottom: 10, textAlign: 'center', fontSize: 15 }}>Joke:</Text>
+        
+        <Text style={{ paddingTop: 10, paddingBottom: 10, textAlign: 'center', fontSize: 25 }}>Jokes for you:</Text>
+        
+        <View>
+        
+        <Button 
+        title="Press me first!"
+        color="red"
+       onPress={ () => Alert.alert(
+        "Warning",
+        "Some jokes contains nsfw, racist, sexist and other improper content. Viewer discretion is advised.",
+        [
+         
+          { text: "OK", onPress: () => console.log("OK Pressed") }
+        ]
+      )}
+        />
+        
+        </View>
+        <View style={styles.separator} />
+        <View style={styles.space} />
+        <Text style={{ paddingTop: 10, paddingBottom: 10, textAlign: 'center', fontSize: 25 }}>Choose your category:</Text>
+
+        <View style={styles.fixToText}>
         <Button style={styles.button}
-          onPress={() => setFetcher("Programming")}
+          onPress={() => setJokeType("Programming")}
           title="Programming"
         />
         <Button style={styles.button}
-          onPress={() => setFetcher("Any")}
-          title="Any"
+          onPress={() => setJokeType("Miscellaneous")}
+          title="Miscellaneous"
         />
+        </View>
+        <View style={styles.space} />
+        <View style={styles.fixToText}>
+        <Button style={styles.button}
+          onPress={() => setJokeType("Dark")}
+          title="Dark"
+        />
+        <Button style={styles.button}
+          onPress={() => setJokeType("Pun")}
+          title="Pun"
+        />
+        </View>
+        
+
+        <View style={styles.separator} />
+
         {isLoading ? <ActivityIndicator /> : (
           <FlatList
             data={[data]}
-            keyExtractor={({ id }, index) => id}
+            keyExtractor={({ id }, index) => index.toString()}
             renderItem={({ item }) => (
               <View>
                 <Text style={{ marginLeft: 5 }}>Category: {item.category} </Text>
+                <Text>  </Text>
                 <Text style={{ marginLeft: 5 }}>{item.joke}</Text>
                 <Text style={{ marginLeft: 5 }}>{item.setup}</Text>
                 <Text style={{ marginLeft: 5 }}>{item.delivery}</Text>
-
               </View>
             )}
           />
@@ -68,8 +106,10 @@ const styles = StyleSheet.create({
     marginRight: 10,
     marginBottom: 10,
     marginTop: 30,
+    
   },
   item: {
+    marginLeft: 5,
     padding: 10,
     fontSize: 10,
     height: 44,
@@ -83,12 +123,18 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginTop: 20,
   },
-  button: {
-    marginBottom: 10,
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
   },
-  row: {
-    flex: 1,
-    padding: 4,
-
+  space: {
+    width: 20,
+    height: 20,
+  },
+  separator: {
+    marginVertical: 10,
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
